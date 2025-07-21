d1749eae3282f1238084e65554ef9220c5fe98c3
Jean-Luc <jluc.blennie@gmail.com>
2025-07-21 23:16:14 +0200

    Ajout de l'icone et lancement Web

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import appJson from './app.json';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <MaterialCommunityIcons name="diving-scuba" size={38} color="#ffd33d" />
+      <Text>{appJson.expo.name} - Open up App.tsx to start working on your app! </Text>
       <StatusBar style="auto" />
     </View>
   );
